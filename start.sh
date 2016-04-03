7fb78d1d27d0b083f7cfa8d3e39bc9eec5acd814
Amon-ra <amon.raj@gmail.com>
2016-04-03 23:43:10 +0200

    add pecl and lts versions

--- start.sh
+++ start.sh
@@ -1,12 +1,24 @@
 #!/bin/dash
 
+source /etc/bashrc
+source ~/.bashrc
+
+#echo "$IP $URL" >> /etc/hosts
+
 if [ "$DEVELOPMENT" != "false" ]
 then
-    hhvm-repo-mode disable
+    #hhvm-repo-mode disable
     echo "" > /etc/hhvm/server.ini
 else
     echo "Initializing"
-    hhvm-repo-mode enable "$ROOT"
+    #hhvm-repo-mode enable "$ROOT"
+    FILE_LIST=$(mktemp)
+    find "/usr/share/php" -type f --name "*.php" > "$FILE_LIST"
+    find "$1" -type f --name "*.php" > "$FILE_LIST"
+    find "$1" -type f --name "*.hh" > "$FILE_LIST"
+
+    OUT_DIR="/var/run/hhvm"
+    hhvm --hphp --target hhbc --output-dir "$OUT_DIR" --input-list "$FILE_LIST" -l3 -v AllVolatile=true    
     echo "hhvm.repo.authoritative = true" > /etc/hhvm/server.ini
 fi
 
